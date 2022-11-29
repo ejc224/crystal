6354e403646156b4eef7d3f2c084c7c58e6b3723
ejc224 <83097225+ejc224@users.noreply.github.com>
2022-11-29 18:06:48 -0500

    Add files via upload

--- World.java
+++ World.java
@@ -1,22 +1,38 @@
+// class World
+
 public class World {
-    static double length, width; // the size of the "glass"
-    public World(double l, double w){
-        length = l;
-        width = w;
+    static boolean debugging = false;
+    public static void main(String[] args){ 
+        // make crystal
+        // in a loop
+            // move, test, move, test
+            // when it returns true, make another particle
+            // move, test, move, test
+            // when it returns true, make another particle
+        Crystal crystal = new Crystal();
+        for (int i=0; i<100; i++){
+            double x = randomCoordinate();
+            double y = randomCoordinate();
+            if (debugging == true)
+                System.out.println("Particle "+i+" with starting coordinates ("+x+", "+y+") has been made");
+            Particle particle = new Particle(x, y);
+            boolean captured = false;
+            int iterations = 0;
+            while(captured == false){ // when true, start at the beginning of the for loop
+                particle.move();
+                captured = crystal.testCapture(particle);
+                iterations++;
+            }
+            if (debugging == true){
+                System.out.println("Iterations = "+ iterations);
+                System.out.println("Size of crystal = "+ crystal.size());
+            }
+        }
+        System.out.println("Size of the crystal = "+ crystal.size());
     }
-    public static void main(String[] args){ // testing if my World is working to be able to check if my particle moves are safe - currently not working
-        World world = new World(5, 5);
-        Particle particle = new Particle(2, 3);
-        Particle compare = new Particle(5,4);
-        System.out.println("Particle starting at: "+particle);
-        particle.move();
-        System.out.println(particle.distance(compare));
-        System.out.println("Particle after move(): "+particle);
-        particle.move();
-        System.out.println(particle.distance(compare));
-        System.out.println("Particle after move(): "+particle);
-        particle.move();
-        System.out.println(particle.distance(compare));
-        System.out.println("Particle after move(): "+particle);
+    // method to generate random x and y values
+    public static double randomCoordinate(){
+        double coord = Math.random()*10;
+        return coord;
     }
 }
